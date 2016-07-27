3065683de1751eafd8c2d6f65810f8aee0fa21a3
Guangwen Xu <guangw.xu@gmail.com>
2016-07-27 02:21:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  elsif num == 2
+    return true
+  elsif num == 3
+    return true
+  elsif num % 2 == 0
+    return false
+  elsif num % 3 == 0
+    return false
+  end
+
+  i = 5
+  until i*i > num
+    if num%i == 0
+       return false
+    end
+      i+=1
+  end
+
+  return true
+
+end
